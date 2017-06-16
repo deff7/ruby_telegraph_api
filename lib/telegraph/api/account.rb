d506e0c0072044efcce5b09dd0f636d90107a73e
Sergey Malenkov <def.sergey@gmail.com>
2017-06-16 17:59:09 +0300

    Add validations for account methods

---
+++ lib/telegraph/api/account.rb
@@ -0,0 +1,19 @@
+module Telegraph
+  module Account
+    def self.create
+
+    end
+
+    def self.edit
+
+    end
+
+    def self.info
+
+    end
+
+    def self.revoke_token
+
+    end
+  end
+end
---
+++ lib/telegraph/validations/account.rb
@@ -0,0 +1,25 @@
+module Telegraph
+  module Account
+    CreateAccountSchema = Dry::Validation.Schema do
+      required(:short_name) { str? & size?(1..32) }
+      optional(:author_name) { str? & size?(0..128) }
+      optional(:author_url) { str? & size?(0..512) }
+    end
+
+    EditAccountInfoSchema = Dry::Validation.Schema do
+      required(:access_token) { str? }
+      optional(:short_name) { str? & size?(1..32) }
+      optional(:author_name) { str? & size?(0..128) }
+      optional(:author_url) { str? size?(0..512) }
+    end
+
+    GetAccountInfoSchema = Dry::Validation.Schema do
+      required(:access_token) { str? }
+      optional(:fields) { array? }
+    end
+
+    RevokeAccessTokenSchema = Dry::Validation.Schema do
+      required(:access_token) { str? }
+    end
+  end
+end
